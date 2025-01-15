52bbf784b03ce4bbcbb0077b673d4b28406845da
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-15 11:05:04 +0500

    Updated Componenets

--- src/components/ShoppingAddForm.jsx
+++ src/components/ShoppingAddForm.jsx
@@ -1,5 +1,11 @@
 const ShoppingAddForm = () => {
-	return <h1>Add form</h1>
+	return (
+		<div className='form'>
+			<input type='text' className='title' placeholder='Title...' />
+			<input type='number' className='number' placeholder='14' />
+			<button>Add</button>
+		</div>
+	)
 }
 
 export default ShoppingAddForm
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,5 +1,29 @@
 const ShoppingList = () => {
-	return <h3>List</h3>
+	return (
+		<div className='shopping-list'>
+			<div className='list-item'>
+				<div className='item-info'>
+					<span>14</span>
+					<p>Buy Bananas</p>
+				</div>
+				<div className='item-actions'>
+					<span className='check'>&#10003;</span>
+					<span className='times'>&times;</span>
+				</div>
+			</div>
+
+			<div className='list-item active'>
+				<div className='item-info'>
+					<span>14</span>
+					<p>Buy Bananas</p>
+				</div>
+				<div className='item-actions'>
+					<span className='check'>&#10003;</span>
+					<span className='times'>&times;</span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ShoppingList
