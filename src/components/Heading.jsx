666159e59a68401dc3a9aad9288c8ca4d4de2fc3
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-15 21:44:47 +0500

    Update

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,25 @@
 const Information = () => {
+	const styleInformation = {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		gap: 10,
+	}
+
+	const styleSpan = {
+		width: 50,
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: 25,
+		color: 'black',
+		padding: 5,
+		borderRadius: 100,
+	}
+
 	return (
-		<div className='information'>
+		<div style={styleInformation} className='information'>
 			<h1>Shopping List</h1>
-			<span>2</span>
+			<span style={styleSpan}>2</span>
 		</div>
 	)
 }
