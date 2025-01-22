8e400090a235c9ac0eb1f6bf69641a6e101bd4fc
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-22 10:46:38 +0500

    Data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,32 @@
+import { Component } from 'react'
 import Filter from './Filter'
 import Heading from './Heading'
 import ShoppingAddForm from './ShoppingAddForm'
 import ShoppingList from './ShoppingList'
-
-function App() {
-	const data = [
-		{ id: 1, size: 14, title: 'Buy bananas', active: false },
-		{ id: 2, size: 10, title: 'Buy apples', active: true },
-		{ id: 3, size: 5, title: 'Buy milk', active: false },
-	]
-	return (
-		<div className='app'>
-			<div className='wrapper'>
-				<div className='card'>
-					<Heading />
-					<ShoppingAddForm />
-					<ShoppingList data={data} />
-					<Filter />
+import { arr } from '/src/constants/index'
+// push the changes
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: arr,
+		}
+	}
+	render() {
+		return (
+			<div className='app'>
+				<div className='wrapper'>
+					<div className='card'>
+						<Heading />
+						<ShoppingAddForm />
+						<ShoppingList data={arr} />
+						<Filter />
+					</div>
+					<img src='/public/earth.svg' alt='earth' />
 				</div>
-				<img src='/public/earth.svg' alt='earth' />
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default App
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const arr = [
+	{ id: 1, size: 14, title: 'Buy bananas', active: false },
+	{ id: 2, size: 10, title: 'Buy apples', active: true },
+	{ id: 3, size: 5, title: 'Buy milk', active: false },
+]
